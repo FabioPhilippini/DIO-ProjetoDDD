edfa2c615a6f3735b8e7cf55cb31a1d71fc16c89
Fabio Philippini <fabioandre381@gmail.com>
2021-01-13 16:26:05 -0300

    Criação das interfaces no services do Domain.Core

---
+++ RestApiDDD.Domain.Core/Interfaces/Services/IServiceCliente.cs
@@ -0,0 +1,11 @@
+﻿using RestApiDDD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestApiDDD.Domain.Core.Interfaces.Services
+{
+    interface IServiceCliente : IServiceBase<Cliente>
+    {
+    }
+}
---
+++ RestApiDDD.Domain.Core/Interfaces/Services/IServiceProduto.cs
@@ -0,0 +1,11 @@
+﻿using RestApiDDD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestApiDDD.Domain.Core.Interfaces.Services
+{
+    interface IServiceProduto : IServiceBase<Produto>
+    {
+    }
+}
