371e390543a7cfa19aad7309bf90aa21fd100828
Fabio Philippini <fabioandre381@gmail.com>
2021-01-13 16:22:29 -0300

    Criação dos repositories na pasta Data

---
+++ RestApiDDD.Infraestructure/Data/Repositories/RepositoryCliente.cs
@@ -0,0 +1,18 @@
+﻿using RestApiDDD.Domain.Core.Interfaces.Repositories;
+using RestApiDDD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestApiDDD.Infraestructure.Data.Repositories
+{
+    public class RepositoryCliente : RepositoryBase<Cliente>, IRepositoryCliente
+    {
+        private readonly SqlContext sqlContext;
+
+        public RepositoryCliente(SqlContext sqlContext) : base(sqlContext)
+        {
+            this.sqlContext = sqlContext;
+        }
+    }
+}
---
+++ RestApiDDD.Infraestructure/Data/Repositories/RepositoryProduto.cs
@@ -0,0 +1,18 @@
+﻿using RestApiDDD.Domain.Core.Interfaces.Repositories;
+using RestApiDDD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestApiDDD.Infraestructure.Data.Repositories
+{
+    public class RepositoryProduto : RepositoryBase<Produto>, IRepositoryProduto
+    {
+        private readonly SqlContext sqlContext;
+
+        public RepositoryProduto(SqlContext sqlContext) : base(sqlContext)
+        {
+            this.sqlContext = sqlContext;
+        }
+    }
+}
