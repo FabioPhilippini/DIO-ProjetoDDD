d69bbf3c26667995ca2538d46b623b40df57f7a9
Fabio Philippini <fabioandre381@gmail.com>
2021-01-13 17:16:22 -0300

    Criação da IOC

---
+++ RestApiDDD.Infraestructure/CrossCutting/IOC/ConfigurationIOC.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+using RestApiDDD.Application;
+using RestApiDDD.Application.Interfaces;
+using RestApiDDD.Application.Interfaces.Mappers;
+using RestApiDDD.Application.Mapper;
+using RestApiDDD.Domain.Core.Interfaces.Repositories;
+using RestApiDDD.Domain.Core.Interfaces.Services;
+using RestApiDDD.Domain.Services;
+using RestApiDDD.Infraestructure.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestApiDDD.Infraestructure.CrossCutting.IOC
+{
+    public class ConfigurationIOC
+    {
+        public static void Load(ContainerBuilder builder)
+        {
+            #region IOC
+            builder.RegisterType<ApplicationServiceCliente>().As<IApplicationServiceCliente>();
+            builder.RegisterType<ApplicationServiceProduto>().As<IApplicationServiceProduto>();
+            builder.RegisterType<ServiceCliente>().As<IServiceCliente>();
+            builder.RegisterType<ServiceProduto>().As<IServiceProduto>();
+            builder.RegisterType<RepositoryCliente>().As<IRepositoryCliente>();
+            builder.RegisterType<RepositoryProduto>().As<IRepositoryProduto>();
+            builder.RegisterType<MapperCliente>().As<IMapperCliente>();
+            builder.RegisterType<MapperProduto>().As<IMapperProduto>();
+
+            #endregion
+        }
+    }
+}
