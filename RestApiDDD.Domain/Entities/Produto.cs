c46c679857c4c8b472c3337b4d3e6f45c8941f98
Fabio Philippini <fabioandre381@gmail.com>
2021-01-13 16:07:50 -0300

    Criação das entidades e do SqlContext

---
+++ RestApiDDD.Domain/Entities/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestApiDDD.Domain.Entities
+{
+    public class Produto : Base
+    {
+        public string Nome { get; set; }
+
+        public double Valor { get; set; }
+
+        public bool IsDisponivel { get; set; }
+    }
+}
